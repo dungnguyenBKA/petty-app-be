3d2ad27a51c40d45bf3d258a826a9d72f2d52ea7
chavisnguyen <dungg.nm@gmail.com>
2022-07-11 02:57:15 +0700

    update

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -15,6 +15,8 @@ import bcrypt from "bcrypt"
 import UserModel from "../models/UserModel.js";
 import {pickPropertyUser} from "../utils/utils.js";
 import {authenticationMiddle} from "../middleware/authenticationMiddle.js";
+import NovelModel from "../models/NovelModel.js";
+import PostModel from "../models/PostModel.js";
 
 const router = express.Router()
 
@@ -30,6 +32,19 @@ async function createUser(username, password) {
 router.get('/', authenticationMiddle, async (req, res) => {
   const users = await UserModel.find({})
   res.json(createResponse(successMessage, users?.map((item) => pickPropertyUser(item))))
+  // let { limit, page } = req.query
+  // const _limit = Number(parseInt(limit) || 10)
+  // const _page = Number(parseInt(page) || 0)
+  //
+  // const novels = await NovelModel.find({})
+  // const startPos = Number(_page*_limit)
+  // const pagingNovels = novels.slice(startPos, startPos+_limit)
+  // const isNextPage = pagingNovels.length === _limit
+  //
+  // res.json(createResponse("success", {
+  //   novels: pagingNovels,
+  //   nextPage: isNextPage ? _page+1 : undefined
+  // }))
 })
 
 router.get('/profile', authenticationMiddle, async (req, res) => {
